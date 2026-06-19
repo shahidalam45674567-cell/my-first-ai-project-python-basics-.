21d3f3e8a434f03de3ff1fa32aae0923fb11a2e8
Shahid devloper <shahidalam45674567@gmail.com>
2026-06-19 19:26:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# My First AI Welcome Project
+
+print("--- AI Developer Assistant ---")
+
+# User se input lena
+name = input("Aapka naam kya hai? ")
+interest = input("Aap kis field me interest rakhte hain (jaise: AI, Web Development)? ")
+
+# Message print karna
+print(f"\nHello {name}!")
+print(f"Great! {interest} seekhna ek bohot accha decision hai. All the best aapke career ke liye!")
